ac31019859ab6091e9072861eb78884571a1e640
Daniel <845765@qq.com>
2024-01-21 21:10:32 +0800

    :bug: Data sync may cause data loss on Android 14 https://github.com/siyuan-note/siyuan/issues/10205

--- go.mod
+++ go.mod
@@ -1,14 +1,35 @@
 module github.com/siyuan-note/filelock
 
-go 1.18
+go 1.21
+
+toolchain go1.21.6
 
 require (
 	github.com/88250/gulu v1.2.3-0.20231209020950-b7b6994e395c
-	github.com/siyuan-note/logging v0.0.0-20231030034701-8265764f00ff
+	github.com/siyuan-note/httpclient v0.0.0-20240105083729-70d0557bc690
+	github.com/siyuan-note/logging v0.0.0-20231208035918-61f884c854f0
 )
 
 require (
-	github.com/getsentry/sentry-go v0.25.0 // indirect
-	golang.org/x/sys v0.15.0 // indirect
+	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/cloudflare/circl v1.3.7 // indirect
+	github.com/getsentry/sentry-go v0.26.0 // indirect
+	github.com/go-task/slim-sprig v0.0.0-20230315185526-52ccab3ef572 // indirect
+	github.com/google/pprof v0.0.0-20240117000934-35fc243c5815 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/imroc/req/v3 v3.42.3 // indirect
+	github.com/klauspost/compress v1.17.4 // indirect
+	github.com/onsi/ginkgo/v2 v2.15.0 // indirect
+	github.com/quic-go/qpack v0.4.0 // indirect
+	github.com/quic-go/quic-go v0.41.0 // indirect
+	github.com/refraction-networking/utls v1.6.1 // indirect
+	go.uber.org/mock v0.4.0 // indirect
+	golang.org/x/crypto v0.18.0 // indirect
+	golang.org/x/exp v0.0.0-20240119083558-1b970713d09a // indirect
+	golang.org/x/mod v0.14.0 // indirect
+	golang.org/x/net v0.20.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/tools v0.17.0 // indirect
 )
---
+++ walk.go
@@ -0,0 +1,118 @@
+// FileLock - Read and write files with lock.
+// Copyright (c) 2022-present, b3log.org
+//
+// FileLock is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//         http://license.coscl.org.cn/MulanPSL2
+//
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+// EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+// MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+//
+// See the Mulan PSL v2 for more details.
+
+package filelock
+
+import (
+	"errors"
+	"fmt"
+	"io/fs"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"time"
+
+	"github.com/siyuan-note/httpclient"
+	"github.com/siyuan-note/logging"
+)
+
+var AndroidServerPort = 6906 // Android HTTP 服务器端口
+
+func Walk(root string, fn filepath.WalkFunc) error {
+	if strings.Contains(runtime.GOOS, "android") {
+		// Android 系统上统一使用 Android HTTP 服务器来遍历文件
+		// Data sync may cause data loss on Android 14 https://github.com/siyuan-note/siyuan/issues/10205
+
+		start := time.Now()
+		req := httpclient.NewCloudFileRequest2m()
+		req.SetBody(map[string]interface{}{"dir": root})
+		resp, err := req.Post("http://127.0.0.1:" + fmt.Sprintf("%d", AndroidServerPort) + "/api/walkDir")
+		logging.LogInfof("walk dir [%s] cost [%s]", root, time.Since(start))
+		if nil != err {
+			logging.LogErrorf("walk dir [%s] failed: %s", root, err)
+			return filepath.Walk(root, fn)
+		}
+		if 200 != resp.StatusCode {
+			logging.LogErrorf("walk dir [%s] failed: %d", root, resp.StatusCode)
+			return filepath.Walk(root, fn)
+		}
+
+		result := map[string]interface{}{}
+		if err = resp.UnmarshalJson(&result); nil != err {
+			logging.LogErrorf("walk dir [%s] failed: %s", root, err)
+			return errors.New("walk dir failed")
+		}
+
+		code := result["code"].(float64)
+		if 0 != code {
+			logging.LogErrorf("walk dir [%s] failed: %f", root, code)
+			return errors.New("walk dir failed")
+		}
+
+		data := result["data"].(map[string]interface{})
+		filesData := data["files"].([]interface{})
+		var infos []*RemoteFile
+		for _, f := range filesData {
+			info := &RemoteFile{info: f.(map[string]interface{})}
+			infos = append(infos, info)
+		}
+
+		for _, info := range infos {
+			p := info.Path()
+			err = fn(p, info, nil)
+			if nil != err {
+				logging.LogErrorf("walk dir [%s] failed: %s", p, err)
+				return err
+			}
+		}
+		return nil
+	}
+	return filepath.Walk(root, fn)
+}
+
+type RemoteFile struct {
+	info map[string]interface{}
+}
+
+func (f *RemoteFile) Path() string {
+	return f.info["path"].(string)
+}
+
+func (f *RemoteFile) Name() string {
+	return f.info["name"].(string)
+}
+
+func (f *RemoteFile) Size() int64 {
+	return int64(f.info["size"].(float64))
+}
+
+func (f *RemoteFile) Mode() fs.FileMode {
+	if f.IsDir() {
+		return 0755
+	}
+	return 0644
+}
+
+func (f *RemoteFile) ModTime() time.Time {
+	ms := int64(f.info["updated"].(float64))
+	return time.UnixMilli(ms)
+}
+
+func (f *RemoteFile) IsDir() bool {
+	return f.info["isDir"].(bool)
+}
+
+func (f *RemoteFile) Sys() interface{} {
+	return nil
+}
